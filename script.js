3e7afee3eebafd5436b745ceef2c14ceb0a30e6f
Rohan Tandale <124422395+RohanTandale@users.noreply.github.com>
2025-12-23 16:21:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+function secondHighest(arr) {
+  if (arr.length < 2) return -Infinity;
+
+  let highest = -Infinity;
+  let second = -Infinity;
+
+  for (let num of arr) {
+    if (num > highest) {
+      second = highest;
+      highest = num;
+    } else if (num < highest && num > second) {
+      second = num;
+    }
+  }
+
+  return second === -Infinity ? -Infinity : second;
+}
